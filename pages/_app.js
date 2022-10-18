b710aaa5ef99c6088c469679be2f1ad871bc7796
cooksey <suitcase24@gmail.com>
2022-10-17 21:53:50 -0400

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from '../components/layout';
 import Head from 'next/head'
+import Script from "next/script"
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
@@ -8,6 +9,23 @@ function App({ Component, pageProps }) {
       <Head>
         <title>Fire!</title>
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-4KM1ERYDXT`}
+        />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4KM1ERYDXT', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
